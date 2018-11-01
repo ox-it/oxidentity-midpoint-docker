a15297a668b5847abb039c1db0c26a09e5cedad5
Robert Bradley <robert.bradley@it.ox.ac.uk>
2018-11-01 18:28:35 +0000

    Spin midPoint database setup off into the database container
    At present, the initial setup of the midPoint database schema occurs every
    time the midpoint container is started.  This is wasteful and potentially
    problematic.  This commit creates a new midpoint-db Docker container that
    inherits from postgres:9.6, but also adds the initial SQL needed to set up
    the database.

--- midpoint/docker-entrypoint.sh
+++ midpoint/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "midpoint-db:5432:midpoint:midpoint:midpoint" > ~/.pgpass
 chmod 600 ~/.pgpass
-psql --host=midpoint-db --username=midpoint -f "/opt/midpoint/doc/config/sql/_all/postgresql-3.8-all.sql"
+#psql --host=midpoint-db --username=midpoint -f "/opt/midpoint/doc/config/sql/_all/postgresql-3.8-all.sql"
 #psql --host=midpoint-db --username=midpoint -f "/opt/midpoint/doc/config/sql/activiti/activiti.postgres.create.engine.sql"
 #psql --host=midpoint-db --username=midpoint -f "/opt/midpoint/doc/config/sql/activiti/activiti.postgres.create.history.sql"
 #psql --host=midpoint-db --username=midpoint -f "/opt/midpoint/doc/config/sql/activiti/activiti.postgres.create.identity.sql"
